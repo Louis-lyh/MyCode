8e1a3cdd8ade691b0d610bdb6652a283818ac6cb
Louis-lyh <74130358+Louis-lyh@users.noreply.github.com>
2023-08-19 23:10:38 +0800

    【学习】插入排序

--- Assets/DataStructure/Sorting.cs
+++ Assets/DataStructure/Sorting.cs
@@ -6,32 +6,65 @@ public class Sorting : MonoBehaviour
 {
     private void Start() 
     {
-        float[] numbers = new float[]{2,2.5f,3,3.5f,1,1.5f,0,0.5f};
+        //乱序数组
+        float[] numbers = new float[]{2,2.5f,3,3.5f,1,0,0.5f,1.1f,1.2f,1.3f,1.4f,1.5f,4.5f,1.6f,1.7f};
+        //冒泡排序
         BubbleSort(numbers);
-        Log(numbers);
+        //插入排序
+        InsertionSort(numbers);
     }
 
     //冒泡排序
     private void BubbleSort(float[] numbers)
     {
+        int count = 0;
         for(int i = 0; i < numbers.Length - 1; i++)
         {
             for(int j = i + 1; j < numbers.Length; j++ )
             {
                 int number01 = (int) numbers[i];
                 int number02 = (int) numbers[j];
+                //前面大于后面 交换
                 if(number01 > number02)
                 {
                     float temp = numbers[i];
                     numbers[i] = numbers[j];
                     numbers[j] = temp;
                 }
+                //次数加一
+                count++;
             }
         }
+
+        Log("冒泡排序",numbers);
+        Debug.Log($"lenght = {numbers.Length} ,count = {count} ,count / Lenght = {count / (float)numbers.Length}");
+    }
+    //插入排序
+    private void InsertionSort(float[] numbers)
+    {
+        for(int i = 1; i < numbers.Length - 1; i++)
+        {
+            for(int j = i; j <  numbers.Length - 1; j--)
+            {
+                int number01 = (int) numbers[j - 1];
+                int number02 = (int) numbers[j];
+                //比前面的小交换
+                if(number02 < number01)
+                {
+                    float temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+                else //比前面的大直接插入下一个
+                    break;
+            }
+        }
+
+         Log("插入排序",numbers);
     }
 
     //打印
-    private void Log(float[] numbers)
+    private void Log(string sortName,float[] numbers)
     {
         string logStr = "";
         for(int i = 0;i < numbers.Length; i++)
@@ -40,6 +73,7 @@ public class Sorting : MonoBehaviour
             if(i < numbers.Length - 1)
                 logStr += ",";
         }
-        Debug.Log(logStr);
+
+        Debug.Log(sortName + " : " + logStr);
     }
 }
