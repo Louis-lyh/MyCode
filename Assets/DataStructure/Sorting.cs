9f2031d79938fda214ee727ec021afbd68017474
Louis-lyh <74130358+Louis-lyh@users.noreply.github.com>
2023-08-21 22:54:49 +0800

    【学习】归并排序

--- Assets/DataStructure/Sorting.cs
+++ Assets/DataStructure/Sorting.cs
@@ -20,7 +20,11 @@ public class Sorting : MonoBehaviour
         float[] numbers03 = new float[numbers.Length];
         numbers.CopyTo(numbers03,0);
         QuickSort(numbers03,0,numbers03.Length - 1);
-         Log("快速排序",numbers03);
+        //归并
+        float[] numbers04 = new float[numbers.Length];
+        numbers.CopyTo(numbers04,0);
+        MergeSort(numbers04,0,numbers04.Length - 1);
+        Log("归并",numbers04);
     }
 
     //冒泡排序
@@ -29,15 +33,15 @@ public class Sorting : MonoBehaviour
         int count = 0;
         for(int i = 0; i < numbers.Length - 1; i++)
         {
-            for(int j = i + 1; j < numbers.Length; j++ )
+            for(int j = 0; j < numbers.Length - 1 - i; j++ )
             {
-                int number01 = (int) numbers[i];
-                int number02 = (int) numbers[j];
+                int number01 = (int) numbers[j];
+                int number02 = (int) numbers[j + 1];
                 //前面大于后面 交换
                 if(number01 > number02)
                 {
-                    float temp = numbers[i];
-                    numbers[i] = numbers[j];
+                    float temp = numbers[j + 1];
+                    numbers[j + 1] = numbers[j];
                     numbers[j] = temp;
                 }
                 //次数加一
@@ -73,22 +77,22 @@ public class Sorting : MonoBehaviour
     //快速排序
     private void QuickSort(float[] numbers,int startIndex,int endIndex)
     {
-        if(startIndex > endIndex)
+        if(startIndex >= endIndex)
             return;
 
         //完成一次排序
         var centerIndex = QuickSortUnit(numbers,startIndex,endIndex);
         //将左边进行排序
-        QuickSortUnit(numbers,startIndex,centerIndex - 1);
+        QuickSort(numbers,startIndex,centerIndex - 1);
         //将右边进行排序
-        QuickSortUnit(numbers,centerIndex,endIndex);
+        QuickSort(numbers,centerIndex + 1,endIndex);
     }
 
     //快速排序
     private int QuickSortUnit(float[] numbers ,int startIndex,int endIndex)
     {
         float key = numbers[startIndex];
-        while(endIndex < startIndex)
+        while(endIndex > startIndex)
         {
             /*从后向前搜索比key小的值*/
             while((int)numbers[endIndex] >= (int)key && endIndex > startIndex)
@@ -105,10 +109,41 @@ public class Sorting : MonoBehaviour
         }
         /*左边都比key小，右边都比key大。//将key放在游标当前位置。//此时startIndex等于endIndex */
         numbers[startIndex] = key;
-        
         return endIndex;
     }
 
+    //归并排序
+    private void MergeSort(float[] numbers,int startIndex,int endIndex)
+    {
+        if(startIndex < endIndex)
+        {
+            int mid = (startIndex + endIndex) / 2;
+            MergeSort(numbers,startIndex,mid);
+            MergeSort(numbers,mid + 1,endIndex);
+            MergeSortUnit(numbers, startIndex, mid, endIndex);
+        }
+    }
+    private void MergeSortUnit(float[] numnbers,int startIndex,int mid,int endIndex)
+    {
+        float[] temp = new float[endIndex - startIndex + 1];
+        int m = startIndex;
+        int n = mid + 1;
+        int k = 0;
+
+        while(n <= endIndex && m <= mid)
+        {
+            if(numnbers[m] > numnbers[n])
+                temp[k++] = numnbers[n++];
+            else
+                temp[k++] = numnbers[m++];
+        }
+        while(n < endIndex + 1)
+            temp[k++] = numnbers[n++];
+        while (m < mid + 1) 
+            temp[k++] = numnbers[m++];
+        for (k = 0, m = startIndex; m < endIndex + 1; k++, m++) 
+            numnbers[m] = temp[k];
+    }
 
 
     //打印
