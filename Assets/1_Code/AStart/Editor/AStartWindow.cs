4028fc45ca2572420487cd4af77eb228be9fd904
LOUIS_PC <785603152@qq.com>
2025-07-15 07:17:56 +0800

    【新增】寻路编辑器

---
+++ Assets/1_Code/AStart/Editor/AStartWindow.cs
@@ -0,0 +1,179 @@
+using System.Collections.Generic;
+using _1_Code.AStart.Editor;
+using UnityEditor;
+using UnityEngine;
+
+public class AStartWindow : EditorWindow
+{
+    // 格子数量
+    private int gridSize = 10;
+    // 滑动窗口位置
+    private Vector2 scrollPosition;
+    // 算法下拉框
+    private int dropdownIndex1 = 0;
+    private string[] dropdownOptions1 = { "A*", "BFS", "Dijkstra" };
+    // 格子类型    
+    private int dropdownIndex2 = 0;
+    private string[] dropdownOptions2 = { "路", "起点", "终点","障碍"};
+
+    // 滑动条进度
+    private float sliderValue = 0.5f;
+    
+    // 地图数据
+    private MapData _mapData;
+    
+    private Dictionary<Color, Texture2D> colorTextures = new Dictionary<Color, Texture2D>();
+    
+    [MenuItem("MyCode/A* Window")]
+    public static void ShowWindow()
+    {
+        GetWindow<AStartWindow>("A*");
+    }
+
+    void OnEnable()
+    {
+        // 初始化地图
+        _mapData = new MapData(gridSize);
+    }
+
+    void OnGUI()
+    {
+        // 左上角下拉框
+        EditorGUILayout.BeginVertical();
+        {
+            // 选择算法 下拉框
+            EditorGUILayout.Space(20);
+            EditorGUILayout.LabelField($"选择算法");
+            dropdownIndex1 = EditorGUILayout.Popup(dropdownIndex1, dropdownOptions1, GUILayout.Width(300));
+            
+            // 格子内线下拉框
+            EditorGUILayout.Space(20);
+            EditorGUILayout.LabelField($"格子类型");
+            dropdownIndex2 = EditorGUILayout.Popup(dropdownIndex2, dropdownOptions2,GUILayout.Width(300));
+        }
+        EditorGUILayout.EndVertical();
+
+        // 滚动视图
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+        {
+            // 网格布局
+            DrawGrid();
+            
+            // 输入框
+            EditorGUILayout.Space(20);
+            DrawCentered(() => {
+                gridSize = EditorGUILayout.IntField("Grid Size", gridSize);
+                gridSize = Mathf.Clamp(gridSize, 1, 50);  // 限制网格大小
+                // 修改大小出现创建地图
+                if(_mapData.Size != gridSize)
+                    _mapData = new MapData(gridSize);
+            });
+            
+            // 滑动条
+            EditorGUILayout.Space(20);
+            DrawCentered(() => {
+                sliderValue = EditorGUILayout.Slider("Slider", sliderValue, 0f, 1f);
+            });
+        }
+        EditorGUILayout.EndScrollView();
+    }
+
+    private void DrawGrid()
+    {
+        EditorGUILayout.Space(10);
+        
+        DrawCentered(() => {
+            GUILayout.Label($"Button Grid ({gridSize}x{gridSize})", EditorStyles.boldLabel);
+            EditorGUILayout.Space(5);
+            
+            // 计算按钮尺寸
+            float btnSize = Mathf.Min(50, (position.width - 40) / gridSize);
+            
+            // 绘制网格
+            for (int y = 0; y < gridSize; y++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                {
+                    // 水平居中
+                    GUILayout.FlexibleSpace();
+                    
+                    for (int x = 0; x < gridSize; x++)
+                    {
+                        var gridType = _mapData.GetMapItemType(x, y);
+                        
+                        Color btnColor = GetButtonColor(gridType);
+                        // 创建按钮样式
+                        GUIStyle btnStyle = new GUIStyle(GUI.skin.button);
+                        btnStyle.normal.background = GetColorTexture(btnColor * 0.7f);
+                        btnStyle.active.background = GetColorTexture(btnColor); // 点击时变亮
+                        
+                        if (GUILayout.Button($"{gridType}", btnStyle,GUILayout.Width(btnSize), GUILayout.Height(btnSize)))
+                        {
+                            // log
+                            Debug.Log($"Clicked button: ({x}, {y}) => {(MapItemType)dropdownIndex2}");
+                            // 修改地图数据
+                            _mapData.ChangeMapData(x, y,(MapItemType)dropdownIndex2);
+                        }
+                    }
+                    
+                    GUILayout.FlexibleSpace();
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+        });
+    }
+
+    // 辅助方法：居中绘制内容
+    private void DrawCentered(System.Action content)
+    {
+        EditorGUILayout.BeginHorizontal();
+        {
+            GUILayout.FlexibleSpace();
+            
+            EditorGUILayout.BeginVertical();
+            {
+                content?.Invoke();
+            }
+            EditorGUILayout.EndVertical();
+            
+            GUILayout.FlexibleSpace();
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+    // 窗口改变大小时重绘
+    void OnInspectorUpdate()
+    {
+        Repaint();
+    }
+
+    private Color GetButtonColor(MapItemType gridType)
+    {
+        switch (gridType)
+        {
+            case MapItemType.Road:
+                return Color.green;
+            case MapItemType.OBS:
+                return Color.black;
+            case MapItemType.Start:
+                return Color.cyan;
+            case MapItemType.End:
+                return Color.yellow;
+        }
+        
+        return Color.white;
+    }
+
+    // 创建纯色纹理
+    private Texture2D GetColorTexture(Color color)
+    {
+        if (!colorTextures.ContainsKey(color))
+        {
+            Texture2D tex = new Texture2D(1, 1);
+            tex.SetPixel(0, 0, color);
+            tex.Apply();
+            colorTextures[color] = tex;
+        }
+        return colorTextures[color];
+    }
+}
---
+++ Assets/1_Code/AStart/Editor/MapData.cs
@@ -0,0 +1,92 @@
+﻿namespace _1_Code.AStart.Editor
+{
+    /// <summary>
+    /// 地图元素
+    /// </summary>
+    public enum MapItemType
+    {
+        Road,   // 路
+        Start, // 起点
+        End,   // 终点
+        OBS,   // 障碍
+    }
+
+    public class MapData
+    {
+        // 数据
+        private MapItemType[][] _mapData;
+        // 起点
+        private int _startIndex;
+        // 终点
+        private int _endIndex;
+        
+        // 尺寸
+        public int Size=> _mapData.Length;
+        
+        public MapData(int mapSize)
+        {
+            _mapData = new MapItemType[mapSize][];
+            for (int i = 0; i < mapSize; i++)
+                _mapData[i] = new MapItemType[mapSize];
+
+            for (int i = 0; i < _mapData.Length; i++)
+            {
+                for (int j = 0; j < _mapData[i].Length; j++)
+                {
+                    _mapData[i][j] = MapItemType.Road;
+                }
+            }
+
+            _startIndex = -1;
+            _endIndex = -1;
+        }
+        
+        /// <summary>
+        /// 修改地图数据
+        /// </summary>
+        public void ChangeMapData(int x,int y, MapItemType mapType)
+        {
+            var index = x + y * _mapData.Length;
+
+            switch (mapType)
+            {
+                case MapItemType.Start:
+                    if (_startIndex >= 0)
+                    {
+                        var startX = _startIndex % _mapData.Length;
+                        var startY = _startIndex / _mapData[0].Length;
+                        _mapData[startX][startY] = MapItemType.Road;
+                    }
+
+                    _startIndex = index;
+                    break;
+                
+                case MapItemType.End:
+                    if (_endIndex >= 0)
+                    {
+                        var endX = _endIndex % _mapData.Length;
+                        var endY = _endIndex / _mapData[0].Length;
+                        _mapData[endX][endY] = MapItemType.Road;
+                    }
+
+                    _endIndex = index;
+                    break;
+                
+            }
+
+            // 记录
+            _mapData[x][y] = mapType;
+        }
+
+        /// <summary>
+        /// 获取地图数据
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public MapItemType GetMapItemType(int x, int y)
+        {
+            return _mapData[x][y];
+        }
+    }
+}
